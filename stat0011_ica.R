0b8c6915eb49e16245db87250b73a5ca896ced72
Jason Zhai <smicjasonzhai@163.com>
2023-02-19 16:34:01 +0000

    General update

--- stat0011_ica.R
+++ stat0011_ica.R
@@ -3,39 +3,43 @@ SP500.temp <- read.csv("data/SP500.csv")
 SP500.price <- as.numeric(gsub(",", "", SP500.temp$Price))
 
 CAC40.temp <- read.csv("data/CAC40.csv")
+CAC40.temp <- t(rev(t(CAC40.temp)))
 CAC40.price <- CAC40.temp$Close
+
+NK225.temp <- read.csv("data/NK225.csv")
+NK225.price <- NK225.temp$Price
 
 # Log return
 SP500 <- log(tail(SP500.price, -1)) - log(head(SP500.price, -1))
 CAC40 <- log(tail(CAC40.price, -1)) - log(head(CAC40.price, -1))
+NK225 <- log(tail(NK225.price, -1)) - log(head(NK225.price, -1))
 
-rm(SP500.temp, CAC40.temp, SP500.price, CAC40.price)
+rm(SP500.temp, CAC40.temp, SP500.price, CAC40.price, NK225.temp, NK225.price)
 
 # Plotting =====================================================================
 
-# Boundaries for plot
-ylim <- range(SP500, CAC40)
-
 # Price trend plot
-plot(SP500, type = "l", ylim = ylim, col = "blue",
-     main = "Price History", ylab = "Price")
-lines(CAC40, type = "l", col = "red")
-
-# Scatter plot
-plot(SP500, CAC40, pch = 20, col = rgb(0,0,0,0.2))
-
-# Histograms
-hist(SP500, breaks = 20, freq = FALSE)
-hist(CAC40, breaks = 20, freq = FALSE)
+# plot(SP500, type = "l", ylim = range(SP500, CAC40), col = "blue",
+#      main = "Price History", ylab = "Price")
+# lines(CAC40, type = "l", col = "red")
+#
+# # Scatter plot
+# plot(SP500, CAC40, pch = 20, col = rgb(0,0,0,0.2))
+#
+# # Histograms
+# hist(SP500, breaks = 20, freq = FALSE)
+# hist(CAC40, breaks = 20, freq = FALSE)
 
 # PIT ==========================================================================
 library(MASS)
 library(KScorrect)
+library(goftest)
 library(fGarch)
 
 # Standard t
 SP500.params.t <- fitdistr(SP500, "t")$estimate
 CAC40.params.t <- fitdistr(CAC40, "t")$estimate
+NK225.params.t <- fitdistr(NK225, "t")$estimate
 
 hist(SP500, breaks = 20, freq = FALSE)
 x <- seq(-0.4, 0.4, 0.001)
@@ -45,16 +49,32 @@ hist(CAC40, breaks = 20, freq = FALSE)
 x <- seq(-0.4, 0.4, 0.001)
 lines(x, dstd(x, CAC40.params.t[1], CAC40.params.t[2]*1.2, CAC40.params.t[3]), type = "l")
 
+hist(NK225, breaks = 20, freq = FALSE)
+x <- seq(-0.4, 0.4, 0.001)
+lines(x, dstd(x, NK225.params.t[1], NK225.params.t[2]*1.2, NK225.params.t[3]), type = "l")
+
 SP500.u.t <- pstd(SP500, SP500.params.t[1], SP500.params.t[2]*1.6, SP500.params.t[3])
 CAC40.u.t <- pstd(CAC40, CAC40.params.t[1], CAC40.params.t[2]*1.2, CAC40.params.t[3])
+NK225.u.t <- pstd(NK225, NK225.params.t[1], NK225.params.t[2]*1.2, NK225.params.t[3])
 
 hist(SP500.u.t)
 hist(CAC40.u.t)
+hist(NK225.u.t)
 
 LcKS(SP500.u.t, cdf = "punif")$p.value
 LcKS(CAC40.u.t, cdf = "punif")$p.value
+LcKS(NK225.u.t, cdf = "punif")$p.value
+ad.test(SP500.u.t, null = "punif")$p.value
+ad.test(CAC40.u.t, null = "punif")$p.value
+ad.test(NK225.u.t, null = "punif")$p.value
+
+
 
 plot(SP500.u.t, CAC40.u.t)
+plot(SP500.u.t, NK225.u.t) # Preferrable combination, grumble copula maybe
+plot(NK225.u.t, CAC40.u.t)
+
+
 model = VineCopula::BiCopSelect(SP500.u.t, CAC40.u.t, familyset = NA, selectioncrit = "AIC", indeptest = TRUE, level = 0.05)
 model
 
